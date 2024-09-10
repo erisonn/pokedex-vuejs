bc4e9f5a2406eea12dc8be8572206dd2f2b72a17
erisonn <erison.frontend@gmail.com>
2024-09-10 15:38:44 -0300

    change main css to scss

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { DefaultApolloClient } from '@vue/apollo-composable'
 import App from './App.vue'
 import router from './router'
 import { apolloClient } from './client'
-import './assets/main.css'
+import './assets/main.scss'
 
 const app = createApp({
   setup() {
