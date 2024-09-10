8162b9a7cac91587543004bbea828ae3c7acacca
erisonn <erison.frontend@gmail.com>
2024-09-10 18:01:16 -0300

    add apploading component

---
+++ src/components/AppLoading/AppLoading.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import LoadingIcon from '../Icons/LoadingIcon.vue'
+</script>
+
+<template>
+  <div class="AppLoading">
+    <LoadingIcon />
+    <p>Loading...</p>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.AppLoading {
+  margin-top: 50px;
+  text-align: center;
+  svg {
+    height: 40px;
+  }
+}
+</style>
