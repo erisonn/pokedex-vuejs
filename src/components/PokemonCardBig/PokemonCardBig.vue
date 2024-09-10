992eeb16fa5ea63fa5d8d77c633b2b2589e7588e
erisonn <erison.frontend@gmail.com>
2024-09-10 18:01:00 -0300

    add pokemoncardbig component

---
+++ src/components/PokemonCardBig/PokemonCardBig.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="PokemonCardBig"></div>
+</template>
+
+<style lang="scss" scoped>
+@import '/src/assets/main.scss';
+.PokemonCardBig {
+  position: sticky;
+  top: 95px;
+  background-color: white;
+  width: 500px;
+  height: 800px;
+  border-radius: 20px;
+  box-shadow: $box-shadow;
+}
+</style>
