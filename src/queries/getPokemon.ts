a55de0c478403dd7e9e6fe334b23f5aeba29eb8e
erisonn <erison.frontend@gmail.com>
2024-09-12 16:33:07 -0300

    move query files to queries folder

---
+++ src/queries/getPokemon.ts
@@ -0,0 +1,26 @@
+import gql from 'graphql-tag'
+
+export const GET_POKEMON = gql`
+  query GetPokemon($pokemonKey: PokemonEnum!) {
+    getPokemon(pokemon: $pokemonKey) {
+      sprite
+      species
+      key
+      types {
+        name
+      }
+      evYields {
+        attack
+        defense
+        hp
+        specialattack
+        specialdefense
+        speed
+      }
+      flavorTexts {
+        game
+        flavor
+      }
+    }
+  }
+`
