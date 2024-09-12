c93458278fbf10198197641a1a698303686f3ea0
erisonn <erison.frontend@gmail.com>
2024-09-12 16:33:41 -0300

    add taglist component

---
+++ src/components/TagList.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import TagListItem from './TagListItem.vue'
+
+interface Props {
+  tags: {
+    name: string
+  }[]
+}
+const props = defineProps<Props>()
+const { tags } = props
+</script>
+
+<template>
+  <div class="TagList">
+    <TagListItem v-for="tag in tags" :key="tag.name" :tag="tag" />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.TagList {
+  display: flex;
+  justify-content: center;
+}
+</style>
