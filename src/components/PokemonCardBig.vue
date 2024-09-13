822c5db565ae3ac56231bda88419e8e556e65775
erisonn <erison.frontend@gmail.com>
2024-09-13 16:56:23 -0300

    implement pokemoncardbigcontent

--- src/components/PokemonCardBig.vue
+++ src/components/PokemonCardBig.vue
@@ -2,30 +2,9 @@
 import { GET_POKEMON } from '@/queries/getPokemon'
 import { useQuery } from '@vue/apollo-composable'
 import { useRoute } from 'vue-router'
-import TagList from './TagList.vue'
 import AppLoading from './AppLoading.vue'
-import { capitalizeFirstLetter } from '@/helpers/capitalizeFirstLetter'
 import { watch } from 'vue'
-import RadarChart from './RadarChart.vue'
-
-interface PageData {
-  sprite: string
-  species: string
-  types: {
-    name: string
-  }[]
-  flavorTexts: {
-    flavor: string
-  }[]
-  evYields: {
-    hp: number
-    attack: number
-    defense: number
-    speed: number
-    specialdefense: number
-    specialattack: number
-  }
-}
+import PokemonCardBigContent from './PokemonCardBigContent.vue'
 
 const route = useRoute()
 const { result, loading, error, refetch } = useQuery(GET_POKEMON, {
@@ -40,62 +19,13 @@ watch(
     })
   }
 )
-
-// const chartCategories = ['HP', 'Attack', 'Defense', 'Speed', 'Sp. Def', 'Sp. Atk']
-// const chartSeries = {
-//   name: '',
-//   data: [
-//     evYields?.hp,
-//     evYields?.attack,
-//     evYields?.defense,
-//     evYields?.speed,
-//     evYields?.specialdefense,
-//     evYields?.specialattack
-//   ]
-// }
 </script>
 
 <template>
   <div class="PokemonCardBig">
     <AppLoading v-if="loading" />
     <p v-else-if="error">Oops! Something went wrong.</p>
-    <div v-else class="Container">
-      <div class="SpriteContainer">
-        <img :src="result.getPokemon.sprite" />
-      </div>
-      <h6>Nº001</h6>
-      <h4>{{ capitalizeFirstLetter(result.getPokemon.species) }}</h4>
-      <TagList :tags="result.getPokemon.types" />
-      <div class="DescriptionSection">
-        <h4>POKÉDEX ENTRY</h4>
-        <p>
-          {{ result.getPokemon.flavorTexts?.[0].flavor }}
-        </p>
-      </div>
-      <div class="ChartSection">
-        <h4>EV YIELDS</h4>
-        <RadarChart
-          v-if="result"
-          chart-id="pokemonEvYieldsChart"
-          :series="[
-            {
-              name: '',
-              data: [
-                result.getPokemon.evYields?.hp,
-                result.getPokemon.evYields?.attack,
-                result.getPokemon.evYields?.defense,
-                result.getPokemon.evYields?.speed,
-                result.getPokemon.evYields?.specialdefense,
-                result.getPokemon.evYields?.specialattack
-              ]
-            }
-          ]"
-          :categories="['HP', 'Attack', 'Defense', 'Speed', 'Sp. Def', 'Sp. Atk']"
-          height="300"
-          width="300"
-        />
-      </div>
-    </div>
+    <PokemonCardBigContent v-else :data="result.getPokemon" />
   </div>
 </template>
 
@@ -114,18 +44,5 @@ watch(
   border: 1px solid $color-grey-transparent;
   border-radius: 8px;
   box-shadow: $box-shadow;
-  .Container {
-    padding: 20px;
-    text-align: center;
-    .SpriteContainer {
-      img {
-        height: 150px;
-      }
-    }
-    .DescriptionSection,
-    .ChartSection {
-      margin-top: 15px;
-    }
-  }
 }
 </style>
