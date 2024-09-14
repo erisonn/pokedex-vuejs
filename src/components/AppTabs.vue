4f39c84197d4bf0be4f4ae00aa15347290318e26
erisonn <erison.frontend@gmail.com>
2024-09-14 16:35:54 -0300

    add tabs component

---
+++ src/components/AppTabs.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+interface Props {
+  tabs: {
+    title: string
+    label: string
+    component: any
+    componentProps: any
+  }[]
+}
+const props = defineProps<Props>()
+const { tabs } = props
+
+const allTabs = ref(tabs)
+const activeTab = ref(tabs[0])
+const changeTab = (newTab: any) => {
+  activeTab.value = newTab
+}
+</script>
+<template>
+  <div class="AppTabs">
+    <div class="Tabs">
+      <button
+        @click="() => changeTab(tab)"
+        :class="{ active: tab.label === activeTab.label }"
+        v-for="tab in allTabs"
+        :key="tab.label"
+      >
+        {{ tab.title }}
+      </button>
+    </div>
+    <component :is="activeTab.component" v-bind="activeTab.componentProps"></component>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import '/src/assets/main.scss';
+.Tabs {
+  margin-bottom: 15px;
+  button {
+    border: 1px solid $color-grey-transparent;
+    border-radius: 4px;
+    padding: 8px;
+    margin-right: 8px;
+    font-size: 12px;
+    font-weight: bold;
+    color: $color-grey-light;
+  }
+  .active {
+    background-color: $color-green-light;
+    color: $color-white;
+  }
+}
+</style>
