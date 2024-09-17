ef42e34b9292b3489b71448a96a5cf20270a8f9a
erisonn <erison.frontend@gmail.com>
2024-09-17 16:02:51 -0300

    add heat map chart

---
+++ src/components/HeatMapChart.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import theme from '../assets/scss/variables.module.scss'
+import { type FormatedTypeMatchups } from '@/helpers/buildHeatMapChartSeries'
+
+interface Props {
+  chartId: string
+  series: FormatedTypeMatchups
+}
+
+const { chartId, series } = defineProps<Props>()
+
+const options = {
+  chart: {
+    fontFamily: theme.FONT_FAMILY,
+    id: chartId,
+    animations: {
+      dynamicAnimation: {
+        enabled: false
+      }
+    }
+  },
+  series: series
+}
+</script>
+
+<template>
+  <div class="HeatMapChart">
+    <apexchart
+      type="heatmap"
+      :options="options"
+      :series="options.series"
+      height="200"
+      width="500"
+    ></apexchart>
+  </div>
+</template>
